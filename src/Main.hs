bd3202c3b274319379f86bdf75e5e3b4e123c6f8
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-05-31 14:53:13 -0700

    adding files

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,18 @@
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import           GHC.Generics
-import           Data.Aeson.Types    hiding (Value)
-import           Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as H
-import           Data.Text           (Text)
 import qualified Data.Text.IO        as T
-import qualified Data.Vector         as V
 import           Text.Karver
-
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as H
-import Data.Text (Text)
-import qualified Data.Text.IO as T
-import qualified Data.Vector as V
+import           Control.Applicative ((<$>))
+import           Text.JsonBib.Types
 
 
 main :: IO ()
 main = do
-  tplStr <- T.readFile "examples/grocery.html"
-  bibStr <- T.readFile "examples/bib.json"
-  let htmlStr = renderTemplate templateHashMap' tplStr
-  T.writeFile "dist/output.html" htmlStr
+  tplStr <- T.readFile tpltF
+  bib    <- readBib    bibF
+  let htmlStr = renderTemplate bib tplStr
+  T.writeFile outF htmlStr
+  where
+    tpltF = "examples/pubs.html"
+    bibF  = "examples/bib.json"
+    outF  = "dist/output.html"
--- src/Text/JsonBib/Types.hs
+++ src/Text/JsonBib/Types.hs
@@ -1,14 +1,36 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Text.JsonBib.Types where
-
+module Text.JsonBib.Types ( readBib, bv ) where
 
 import           GHC.Generics
 import           Data.Aeson.Types    hiding (Value)
 import           Data.Text           (Text)
+import           Data.Aeson          (eitherDecode')
 import           Text.Karver         (Value)
+import qualified Data.HashMap.Strict as H
+import           Control.Applicative ((<$>))
+import qualified Data.ByteString.Lazy as LB
 
+type BibValue = H.HashMap Text Value
+
+readBib :: FilePath -> IO BibValue
+readBib f = do
+  b <- eitherDecode' <$> LB.readFile f
+  case b of
+    Left err -> error err
+    Right v  -> return $ bibValue v
+
+bibValue :: Bib -> BibValue
+bibValue b = H.fromList [ ("pubs", value $ pubs b)]
+
+value :: (ToJSON a) => a -> Value
+value z = case mkValue z of
+            Error s   -> error $ "Error converting from JSON:" ++ s
+            Success s -> s
+  where
+    mkValue :: (ToJSON a) => a -> Result Value
+    mkValue = fromJSON . toJSON
 
 data Pub = Pub {
     name       :: Text
@@ -25,15 +47,9 @@ data Bib = Bib {
   }
   deriving (Eq, Ord, Show, Generic)
 
-instance ToJSON Pub
-instance ToJSON Bib
+instance FromJSON Pub
+instance FromJSON Bib
+instance ToJSON   Pub
+instance ToJSON   Bib
 
-
-value :: (ToJSON a) => a -> Value
-value z = case mkValue z of
-            Error s   -> error $ "Error converting from JSON:" ++ s
-            Success s -> s
-  where
-    mkValue :: (ToJSON a) => a -> Result Value
-    mkValue = fromJSON . toJSON
 
